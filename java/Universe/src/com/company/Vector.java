215cf7ebc1bafbb26970b7ecc90d07bdb490511a
SanyakaMLG <95969522+SanyakaMLG@users.noreply.github.com>
2022-03-16 21:42:16 +0300

    Add files via upload

---
+++ java/Universe/src/com/company/Vector.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Vector {
+    private double x, y, z;
+    public Vector (double inX, double inY, double inZ) {
+        this.x = inX;
+        this.y = inY;
+        this.z = inZ;
+    }
+    public double getX() {
+        return this.x;
+    }
+    public double getY() {
+        return this.x;
+    }
+    public double getZ() {
+        return this.z;
+    }
+}
