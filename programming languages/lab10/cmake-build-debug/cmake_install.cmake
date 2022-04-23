779afd52c065ff4bfa88a1681516fb8f2f8f2b7e
SanyakaMLG <alekseev.sasha0204@yandex.ru>
2022-04-23 17:40:27 +0300

    refactor

--- programming languages/lab10/cmake-build-debug/cmake_install.cmake
+++ programming languages/lab10/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/aleks/Desktop/lab10
+# Install script for directory: C:/Users/aleks/Desktop/BMSTU_projects/programming languages/lab10
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/aleks/Desktop/lab10/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/aleks/Desktop/BMSTU_projects/programming languages/lab10/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
